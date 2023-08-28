e29ec2ad072cce30fed5e4140c1e6afff9fbb697
Cleve <clevechan@gmail.com>
2023-08-28 21:07:01 +0800

    added large image to match thumbnail images

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,17 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails = document.querySelector("#gallery-thumbs").
+                            querySelectorAll("img");
+  let mainimage = document.querySelector("#gallery-photo img");
+
+  thumbnails.forEach(function(thumbnail) {
+    //Set clicked image as main image
+    thumbnail.addEventListener("click", function() {
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainimage.setAttribute("src", newImageSrc);
+    });
+  });
 }
